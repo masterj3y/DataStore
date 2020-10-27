caffa938041168fe8abfc6e9a14759883da8af85
Mohammad Jahangiry <mjahangiry75@gmail.com>
2020-10-27 18:33:04 +0330

    Initial commit

---
+++ app/src/main/java/io/github/masterj3y/MainActivity.kt
@@ -0,0 +1,38 @@
+package io.github.masterj3y
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import io.github.masterj3y.ui.DataStoreTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DataStoreTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DataStoreTheme {
+        Greeting("Android")
+    }
+}
